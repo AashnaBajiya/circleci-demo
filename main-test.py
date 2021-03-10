5c2685bc73d6d5c589f8f4cd234558764a15233f
AashnaBajiya <79082319+AashnaBajiya@users.noreply.github.com>
2021-03-10 13:28:50 +0530

    Update main-test.py

--- main-test.py
+++ main-test.py
@@ -5,6 +5,7 @@ def TestAdd():
         assert Add(2,3) == 5
         assert Add(5,5) == 10
         print("Add Function works correctly")
+        print("hello")
 
 if __name__ == '__main__':
         TestAdd()
